8bae0beb18221affdb4de01c34f3d5da6a39f6e0
Roland Bernard <rolli.bernard@gmail.com>
2021-12-06 23:57:26 +0100

    Added typescript solutions for day 6

---
+++ 6.lanternfish/ts/part1.ts
@@ -0,0 +1,22 @@
+
+import { readFileSync } from 'fs';
+
+const text = readFileSync(process.stdin.fd).toString()
+const numbers = text.split(',').filter(v => v).map(v => parseInt(v))
+
+const state = new BigInt64Array(9);
+for (const n of numbers) {
+    state[n]++;
+}
+
+for (let i = 0; i < 80; i++) {
+    const births = state[0];
+    for (let i = 1; i < 9; i++) {
+        state[i - 1] = state[i];
+    }
+    state[6] += births;
+    state[8] = births;
+}
+
+console.log('Result:', state.reduce((a, b) => a + b));
+
