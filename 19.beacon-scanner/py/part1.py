18c30981253d416b02b01f56423f5e7e71d90644
Roland Bernard <rolli.bernard@gmail.com>
2021-12-19 16:05:14 +0100

    Solved day 19

---
+++ 19.beacon-scanner/py/part1.py
@@ -0,0 +1,63 @@
+
+import sys
+import numpy as np
+
+def parseScanner(s: str):
+    split = s.split('\n')
+    bacons = [tuple(int(v) for v in b.split(',')) for b in split[1:]]
+    return bacons
+
+scanners = [parseScanner(s) for s in sys.stdin.read().strip().split('\n\n')]
+
+def rotate(d, a):
+    sin = [0, 1, 0, -1]
+    cos = [1, 0, -1, 0]
+    mat = [
+        [1, 0, 0],
+        [0, cos[d], -sin[d]],
+        [0, sin[d], cos[d]]
+    ]
+    return np.roll(mat, a, (0, 1))
+
+def posibleRotations():
+    for y in range(4):
+        for x in range(4):
+            yield np.dot(rotate(x, 0), rotate(y, 1))
+    for z in [-1, 1]:
+        for x in range(4):
+            yield np.dot(rotate(x, 0), rotate(z, 2))
+
+def detectOverlap(a, b):
+    for rot in posibleRotations():
+        rotateb = [tuple(np.dot(rot, p)) for p in b]
+        for i in a:
+            for j in rotateb:
+                pos = (i[0] - j[0], i[1] - j[1], i[2] - j[2])
+                movedb = [(x + pos[0], y + pos[1], z + pos[2]) for x, y, z in rotateb]
+                seta = set(a)
+                setb = set(movedb)
+                if len(seta & setb) >= 12:
+                    return (True, pos, rot)
+    return (False, None, None)
+
+def applyTransform(trans, rot, point):
+    rotated = np.dot(rot, point)
+    return (rotated[0] + trans[0], rotated[1] + trans[1], rotated[2] + trans[2])
+
+points = set(scanners[0])
+positions = {0: (0, 0, 0)}
+missing = set(range(1, len(scanners)))
+
+while len(missing) != 0:
+    for k, p in positions.copy().items():
+        for i in missing.copy():
+            ovl, pos, rot = detectOverlap(scanners[k], scanners[i])
+            if ovl:
+                apos = (p[0] + pos[0], p[1] + pos[1], p[2] + pos[2])
+                points.update(applyTransform(apos, rot, p) for p in scanners[i])
+                missing.remove(i)
+                positions[i] = apos
+                scanners[i] = [tuple(np.dot(rot, p)) for p in scanners[i]]
+
+print("Result:", len(points))
+
